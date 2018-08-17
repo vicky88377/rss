b0980169041a19548be28bc1b25aad2420480668
M1046571 <M1046571@A2ML10704.mindtree.com>
2018-08-17 11:31:25 +0530

    Resturent search service maven project structure

---
+++ restaurant-search-service/src/main/java/com/mindtree/restaurantsearchservice/RestaurantSearchServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mindtree.restaurantsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantSearchServiceApplication.class, args);
+	}
+}
