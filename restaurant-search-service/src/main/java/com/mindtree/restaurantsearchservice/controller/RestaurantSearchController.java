0fa4f2b4ead5b1a58d7c8f5eb88ebf18ca545032
Abhi <abhi.go@hotmail.com>
2018-08-21 19:06:28 +0530

    Added apis in controller

--- restaurant-search-service/src/main/java/com/mindtree/restaurantsearchservice/controller/RestaurantSearchController.java
+++ restaurant-search-service/src/main/java/com/mindtree/restaurantsearchservice/controller/RestaurantSearchController.java
@@ -1,10 +1,40 @@
 package com.mindtree.restaurantsearchservice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mindtree.restaurantsearchservice.model.ResponseStatusModel;
 
 @RestController
 @RequestMapping("/restaurants")
 public class RestaurantSearchController {
 
+	@GetMapping("/search/{area}")
+	public ResponseStatusModel searchRestaurantByArea(@PathVariable String area,@RequestParam String name,@RequestParam int page,@RequestParam String rating,@RequestParam String budget,
+			@RequestParam String cuisine) {
+		return null;
+	}
+	
+	@GetMapping("/search/{latitude}/{longitude}")
+	public ResponseStatusModel searchRestaurantByCoordinates(String latitude,String longitude,@RequestParam String name,@RequestParam(defaultValue="1") int page,@RequestParam String rating,@RequestParam String budget,
+			@RequestParam String cuisine) {
+		return null;
+	}
+	
+	@GetMapping("{restaurant_id}")
+	public ResponseStatusModel getRestaurantDetailsById(@PathVariable("restaurant_id") String restaurantId) {
+		return null;
+	}
+	@GetMapping("/{restaurant_id}/food/{food_id}")
+	public ResponseStatusModel getFoodDetailsByFoodId(@RequestParam("restaurant_id") String restaurantId,@RequestParam("food_id") String foodId) {
+		return null;
+	}
+	
+	@GetMapping("/{restaurant _id}/validate/{latitude}/{longitude}")
+	public ResponseStatusModel validateDeliveryAddress(@PathVariable String latitude,@PathVariable String longitude) {
+		return null;
+	}
 }
