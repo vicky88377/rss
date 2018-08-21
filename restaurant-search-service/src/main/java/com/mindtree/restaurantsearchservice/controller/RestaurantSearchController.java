3fc3880a7a23901489c1b4a4188e18bbece23a20
Abhi <abhi.go@hotmail.com>
2018-08-21 12:54:18 +0530

    Added Response Model classes

--- restaurant-search-service/src/main/java/com/mindtree/restaurantsearchservice/controller/RestaurantSearchController.java
+++ restaurant-search-service/src/main/java/com/mindtree/restaurantsearchservice/controller/RestaurantSearchController.java
@@ -1,6 +1,5 @@
 package com.mindtree.restaurantsearchservice.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
