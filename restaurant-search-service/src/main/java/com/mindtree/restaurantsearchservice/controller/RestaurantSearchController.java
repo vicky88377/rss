bafc5637faec959fd401bd51e242ff704294f448
Abhi <abhi.go@hotmail.com>
2018-08-21 12:03:48 +0530

    created restaurant & response model class

---
+++ restaurant-search-service/src/main/java/com/mindtree/restaurantsearchservice/controller/RestaurantSearchController.java
@@ -0,0 +1,11 @@
+package com.mindtree.restaurantsearchservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurantSearchController {
+
+}
