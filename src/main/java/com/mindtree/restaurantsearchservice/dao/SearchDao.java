a72a21d95e3da234464796f608e3b5aa1993099c
M1046726 <M1046726@A2ML17979.mindtree.com>
2018-08-23 15:26:23 +0530

    DAO API implemented

--- src/main/java/com/mindtree/restaurantsearchservice/dao/SearchDao.java
+++ src/main/java/com/mindtree/restaurantsearchservice/dao/SearchDao.java
@@ -1,5 +1,28 @@
 package com.mindtree.restaurantsearchservice.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.mindtree.restaurantsearchservice.model.RestaurantModel;
+
 public interface SearchDao {
+	
+	public Page<RestaurantModel> findByAreaRatingBudgetDAO(String area, float rating, float minimumOrderPrice,
+			Pageable page);
+	
+	public Page<RestaurantModel> findByAreaAndCuisineDAO(String area, String cuisineType, float rating,
+			float minimumOrderPrice, Pageable page);
+	
+	public Page<RestaurantModel> findByAreaAndNameDAO(String area, String restaurantName, Pageable page);
+	
+	public Page<RestaurantModel> findByIdDAO(String id, Pageable page);
+	
+	public Page<RestaurantModel> findByLonAndLatDAO(float distance, double latitude, double longitude, Pageable page);
+
+	public Page<RestaurantModel> findByLonLatAndNameDAO(String restaurantName, float distance, double latitude,
+			double longitude, Pageable page);
+	
+	public Page<RestaurantModel> findByLonLatRatingBudgetDAO(String cuisineType, float rating, float minimumOrderPrice,
+			float distance, double latitude, double longitude, Pageable page);
 	
 }
