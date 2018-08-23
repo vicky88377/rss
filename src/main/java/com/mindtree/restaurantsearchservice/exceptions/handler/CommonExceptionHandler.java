eb3be6a111275a2c1d6b02c5270dc1b429fa3897
M1046464 <M1046464@A2ML27485.mindtree.com>
2018-08-23 13:19:25 +0530

    changes

--- src/main/java/com/mindtree/restaurantsearchservice/exceptions/handler/CommonExceptionHandler.java
+++ src/main/java/com/mindtree/restaurantsearchservice/exceptions/handler/CommonExceptionHandler.java
@@ -1,11 +1,12 @@
 package com.mindtree.restaurantsearchservice.exceptions.handler;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.mindtree.restaurantsearchservice.exceptions.DataAccessException;
+
 
 @RestControllerAdvice
 public class CommonExceptionHandler {
