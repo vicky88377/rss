35a42f1576bb5c1f17955aa053398e7f5bd16371
M1046726 <M1046726@A2ML17979.mindtree.com>
2018-08-23 15:15:17 +0530

    Created Repository API - byId

--- src/main/java/com/mindtree/restaurantsearchservice/RestaurantSearchServiceApplication.java
+++ src/main/java/com/mindtree/restaurantsearchservice/RestaurantSearchServiceApplication.java
@@ -18,15 +18,17 @@ public class RestaurantSearchServiceApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Pageable pageable = new PageRequest(0, 10);
-		//Page<RestaurantModel> data = repo.findByAreaRatingBudget("Bangalore", 3.0f, 200.0f, pageable);
-		//Page<RestaurantModel> data = repo.findByLonAndLat(5, -47.95628333, -15.83451389, pageable);
-		//Page<RestaurantModel> data = repo.findByLonLatAndName("Steakhouse",5, -47.95628333, -15.83451389, pageable);
-		Page<RestaurantModel> data = repo.findByLonLatRatingBudget("North Indian",3,200,1, 77.696664, 12.97537691, pageable);
-		data.stream().forEach(System.out::println);
-	
-		
+		// Page<RestaurantModel> data = repo.findByAreaRatingBudget("Bangalore", 3.0f, 200.0f, pageable);
+		// Page<RestaurantModel> data = repo.findByAreaAndCuisine("Bangalore", "North Indian", 3.0f, 200.0f, pageable);
+		// Page<RestaurantModel> data = repo.findByAreaAndName("Bangalore", "Sultans of Spice", pageable);
+		// Page<RestaurantModel> data = repo.findById("50943", pageable);
+		// Page<RestaurantModel> data = repo.findByLonAndLat(5, -47.95628333, -15.83451389, pageable);
+		// Page<RestaurantModel> data = repo.findByLonLatAndName("Steakhouse",5, -47.95628333, -15.83451389, pageable);
+		// Page<RestaurantModel> data = repo.findByLonLatRatingBudget("North Indian",3,200,1, 77.696664, 12.97537691, pageable);
+		//data.stream().forEach(System.out::println);
+
 	}
 
 	public static void main(String[] args) {
--- src/main/java/com/mindtree/restaurantsearchservice/repository/RestaurantSearchRepository.java
+++ src/main/java/com/mindtree/restaurantsearchservice/repository/RestaurantSearchRepository.java
@@ -8,27 +8,40 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import com.mindtree.restaurantsearchservice.model.RestaurantModel;
 
 public interface RestaurantSearchRepository extends ElasticsearchRepository<RestaurantModel, String> {
-	
-	// Getting restaurant details by area(mandatory), rating and budget(optional) parameters
-	@Query("{\"bool\":{\"must\":[{\"multi_match\":{\"query\":\"?0\",\"fields\":[\"city\",\"address\",\"locality\"]}},{\"range\":{\"rating\":{\"lte\":5,\"gte\":?1}}},{\"range\":{\"minimum_order_price\":{\"gte\":?2}}}]}},\"sort\":[{\"rating\":{\"order\":\"desc\"}}]")
-	public Page<RestaurantModel> findByAreaRatingBudget(String area, float rating, float minimumOrderPrice, Pageable page);
 
-	// Getting restaurant details by area as well as cuisine_type(mandatory), rating and budget(optional) parameters
+	// Getting restaurant details by area(mandatory), rating and
+	// budget(optional) parameters
+	@Query("{\"bool\":{\"must\":[{\"multi_match\":{\"query\":\"?0\",\"fields\":[\"city\",\"address\",\"locality\"]}},{\"range\":{\"rating\":{\"lte\":5,\"gte\":?1}}},{\"range\":{\"minimum_order_price\":{\"gte\":?2}}}]}},\"sort\":[{\"rating\":{\"order\":\"desc\"}}]")
+	public Page<RestaurantModel> findByAreaRatingBudget(String area, float rating, float minimumOrderPrice,
+			Pageable page);
+
+	// Getting restaurant details by area as well as cuisine_type(mandatory),
+	// rating and budget(optional) parameters
 	@Query("{\"bool\":{\"must\":[{\"multi_match\":{\"query\":\"?0\",\"fields\":[\"city\",\"address\",\"locality\"]}},{\"match\":{\"cuisen_type\":\"?1\"}},{\"match_phrase\":{\"cuisen_type\":\"?1\"}},{\"range\":{\"rating\":{\"lte\":5,\"gte\":?2}}},{\"range\":{\"minimum_order_price\":{\"gte\":?3}}}]}},\"sort\":[{\"rating\":{\"order\":\"desc\"}}]")
-	public Page<RestaurantModel> findByAreaAndCuisine(String area, String cuisineType, float rating, float minimumOrderPrice, Pageable page);
-	
-	//Getting restaurant details by location and distance
+	public Page<RestaurantModel> findByAreaAndCuisine(String area, String cuisineType, float rating,
+			float minimumOrderPrice, Pageable page);
+
+	// Getting restaurant details by area and restaurant name
+	@Query("{\"bool\":{\"should\":[{\"multi_match\":{\"query\":\"?0\",\"fields\":[\"city\",\"address\",\"locality\"]}}],\"must\":[{\"match_phrase\":{\"restaurant_name\":{\"query\":\"?1\",\"boost\":3}}},{\"match\":{\"restaurant_name\":{\"query\":\"?1\"}}}]}}")
+	public Page<RestaurantModel> findByAreaAndName(String area, String restaurantName, Pageable page);
+
+	// Getting restaurant my restaurant id
+	@Query("{\"match\":{\"restaurant_id\":\"?0\"}}")
+	public Page<RestaurantModel> findById(String id, Pageable page);
+
+	// Getting restaurant details by location and distance
 	@Query("{\"bool\":{\"filter\":{\"geo_distance\":{\"distance\":\"?0km\",\"location\":{\"lat\":?1,\"lon\":?2}}}}}")
 	public Page<RestaurantModel> findByLonAndLat(float distance, double latitude, double longitude, Pageable page);
 
 	// Getting restaurant details by restaurant name, location & distance
 	@Query("{\"bool\":{\"must\":[{\"match\":{\"restaurant_name\":\"?0\"}},{\"match_phrase\":{\"restaurant_name\":\"?0\"}}],\"filter\":{\"geo_distance\":{\"distance\":\"?1km\",\"location\":{\"lat\":?2,\"lon\":?3}}}}}")
-	public Page<RestaurantModel> findByLonLatAndName(String restaurantName, float distance, double latitude, double longitude,
-			Pageable page);
-	
-	// Getting restaurant details by location, distance & cuisine_type(mandatory), rating and budget(optional) parameters
+	public Page<RestaurantModel> findByLonLatAndName(String restaurantName, float distance, double latitude,
+			double longitude, Pageable page);
+
+	// Getting restaurant details by location, distance &
+	// cuisine_type(mandatory), rating and budget(optional) parameters
 	@Query("{\"bool\":{\"must\":[{\"match\":{\"cuisen_type\":\"?0\"}},{\"match_phrase\":{\"cuisen_type\":\"?0\"}},{\"range\":{\"rating\":{\"lte\":5,\"gte\":?1}}},{\"range\":{\"minimum_order_price\":{\"gte\":?2}}}],\"filter\":{\"geo_distance\":{\"distance\":\"?3km\",\"location\":{\"lat\":?4,\"lon\":?5}}}}},\"sort\":[{\"rating\":{\"order\":\"desc\"}}]")
-	public Page<RestaurantModel> findByLonLatRatingBudget(String cuisineType, float rating, float minimumOrderPrice, float distance, double latitude, double longitude,
-			Pageable page);
+	public Page<RestaurantModel> findByLonLatRatingBudget(String cuisineType, float rating, float minimumOrderPrice,
+			float distance, double latitude, double longitude, Pageable page);
 
 }
